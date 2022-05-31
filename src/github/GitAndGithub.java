9850d20eb3c7fb73016099e6510aa4a7af066c3b
maratkhanovna <maratkhanovna@gmail.com>
2022-05-31 19:55:14 -0400

    2nd commit

--- src/github/GitAndGithub.java
+++ src/github/GitAndGithub.java
@@ -4,6 +4,9 @@ public class GitAndGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Today we learn github");
+		System.out.println("Today is a very good day");
 
 	}
 
